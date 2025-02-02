b7226f92eb25f63176f351e4698841e4b12e19f2
AlyHamzay <ali.hamaza7979@gmail.com>
2025-02-03 01:57:01 +0500

    Updated frontend code and removed unused files

--- Front-end/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { useRoutes } from "react-router";
-import routes from "./routes";
-
-function App() {
-  const element = useRoutes(routes);  // Create elements based on the current route
-  return <>{element}</>;  // Render the elements
-}
-
-export default App;
--- Front-end/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';  // This will continue working in JavaScript
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
